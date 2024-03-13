c045610348474cc2fba8ca275533bc1d84c54e9f
Plouzeau Alexandre <plouzeau.alexandre@hotmail.fr>
2024-03-13 16:40:23 +0100

    Create repos

---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100&family=Titillium+Web:wght@200&family=Ubuntu+Mono:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <title>Accueil</title>
+</head>
+
+<body>
+    <div class="container-fluid">
+
+        <header>
+            <h1 class="title">SOPEYO</h1>
+            <ul class="nav justify-content-center">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#">Devis</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Comptabilité</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Résultats</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Chantiers</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Ressources</a>
+                </li>
+            </ul>
+        </header>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <script src="script.js"></script>
+</body>
+
+</html>
